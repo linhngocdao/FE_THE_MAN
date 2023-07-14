d4aa878d1fa93733f0aa97ec2964fc242efa00f8
linhngocdao <linhngocdao22@gmail.com>
2023-07-14 18:17:26 +0700

    push api render

--- src/api-cilent/Config.tsx
+++ src/api-cilent/Config.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://datn-8e48.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
